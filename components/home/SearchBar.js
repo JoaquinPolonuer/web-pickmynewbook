78f5d8387f20a566bb134cc285a0b3ce1ce0f00a
joaquinpolonuer <jtpolonuer@gmail.com>
2021-08-09 19:39:00 -0300

    added content to homepage

---
+++ components/home/SearchBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "../../styles/SearchBar.module.css";
+
+export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      matches: [],
+    };
+  }
+
+  getMatches = () => {
+    fetch("http://localhost:8000/book-recommender/" + this.state.search).then(
+      console.log((res) => console.log(res.json()))
+    );
+  };
+
+  handleChange = (e) => {
+    this.setState({ search: e.target.value }, () => {
+      if (this.state.search.length > 1) {
+        this.getMatches();
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          className={styles.search}
+          type="text"
+          placeholder="I want a book similar to..."
+          value={this.state.search}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
